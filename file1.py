1461faba6d47aa95c4e52ec8fdc7698a7a10c457
chakrapanishroff <chakrapanishroff@gmail.com>
2023-08-02 22:19:11 +0530

    Initial commit

---
+++ file1.py
@@ -0,0 +1,6 @@
+# file1.py - Original version
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
---
+++ file2.py
@@ -0,0 +1,6 @@
+# file2.py - Modified version
+def add(a, b):
+    return a + b
+
+def multiply(a, b):  # New function added
+    return a * b
