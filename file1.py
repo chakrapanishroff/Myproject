4199e4039ac6f79d9e5c6e2ceaeace0634f94953
chakrapanishroff <chakrapanishroff@gmail.com>
2023-08-02 22:26:18 +0530

    Removed unncessary files

--- file1.py
+++
@@ -1,6 +0,0 @@
-# file1.py - Original version
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
--- file2.py
+++
@@ -1,6 +0,0 @@
-# file2.py - Modified version
-def add(a, b):
-    return a + b
-
-def multiply(a, b):  # New function added
-    return a * b
